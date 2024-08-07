6780712842d1b6344d30005e33c711272f9cebc8
Pat <patb.software@gmail.com>
2024-08-07 21:58:01 +0700

    Create Task schema for backend

---
+++ backend/src/main/java/com/example/backend/task/Task.java
@@ -0,0 +1,43 @@
+package com.example.backend.task;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String description;
+
+    private boolean completed;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
