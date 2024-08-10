acaec96904df39154c6dd6fd4db6cb0a9009337e
Pat <patb.software@gmail.com>
2024-08-10 13:15:22 +0700

    Add PATCH and DELETE endpoints for tasks

--- backend/src/main/java/com/example/backend/task/TaskController.java
+++ backend/src/main/java/com/example/backend/task/TaskController.java
@@ -1,12 +1,17 @@
 package com.example.backend.task;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 
 
@@ -30,5 +35,22 @@ public class TaskController {
         return taskRepository.save(task);
     }
     
-    
+    // Updates the task by setting the completed field to true
+    @PatchMapping("/{id}")
+    public Task completeTaskById(@PathVariable String id) {
+        Optional<Task> taskToUpdateOptional = taskRepository.findById(id);
+
+        if (taskToUpdateOptional.isPresent()) {
+            Task taskToUpdate = taskToUpdateOptional.get();
+            taskToUpdate.setCompleted(true);
+            return taskRepository.save(taskToUpdate);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTaskById(@PathVariable String id) {
+        taskRepository.deleteById(id);
+    }
 }
